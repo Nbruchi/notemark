2c07a4158ad36498df58cb29251d71571ba33f02
Nbruchi <nkundabagenzibruce@gmail.com>
2024-11-14 05:49:26 +0200

    notemark

---
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -0,0 +1,35 @@
+import { NotePreview } from '@/components'
+import { useNotesList } from '@renderer/hooks/useNotesList'
+import { isEmpty } from 'lodash'
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export type NotePreviewListProps = ComponentProps<'ul'> & {
+  onSelect?: () => void
+}
+
+export const NotePreviewList = ({ onSelect, className, ...props }: NotePreviewListProps) => {
+  const { notes, selectedNoteIndex, handleNoteSelect } = useNotesList({ onSelect })
+
+  if (!notes) return null
+
+  if (isEmpty(notes)) {
+    return (
+      <ul className={twMerge('text-center pt-4', className)} {...props}>
+        <span>No notes yet!</span>
+      </ul>
+    )
+  }
+  return (
+    <ul {...props} className={className}>
+      {notes.map((note, index) => (
+        <NotePreview
+          key={note.title + note.lastEditTime}
+          {...note}
+          isActive={selectedNoteIndex === index}
+          onClick={handleNoteSelect(index)}
+        />
+      ))}
+    </ul>
+  )
+}
---
+++ src/shared/constants.ts
@@ -0,0 +1,5 @@
+export const appDirectory = `NoteMark`
+export const fileEncoding = 'utf8'
+
+export const autoSavingTime = 1000
+export const welcomeNoteFilename = 'Welcome.md'
